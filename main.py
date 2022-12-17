be4f0152c8e34df872a4a92aec9bd1d6d6248608
Dennis <1824116@stud.hs-mannheim.de>
2022-12-17 22:35:04 +0100

    adding only pdf creation and select number of prints

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import random
 import string
 import subprocess
-from tkinter import Tk, IntVar, Label, W, Radiobutton, Button, mainloop, TOP, BOTTOM, Frame, LEFT, CENTER, RIGHT, \
+from tkinter import Tk, IntVar, Label, Entry, W, Radiobutton, Button, mainloop, TOP, BOTTOM, Frame, LEFT, CENTER, RIGHT, \
     StringVar, OptionMenu
 from tkinter.filedialog import askopenfilenames
 from PyPDF2 import PdfFileReader, PdfFileWriter
@@ -22,7 +22,7 @@ if not filenames:
 
 master = Tk()
 master.eval('tk::PlaceWindow . center')
-master.geometry('250x150')
+master.geometry('250x200')
 var = IntVar(master)
 var.set(0)
 
@@ -50,6 +50,7 @@ middleFrame = Frame(master)
 
 printerList = subprocess.getoutput("lpstat -p | awk '{print $2}'")
 printerList = printerList.split("\n")
+printerList.append("PDF")
 
 printer = StringVar(master)
 firstDisplayedPrinter = printerList[0]
@@ -67,6 +68,11 @@ Radiobutton(master, text="Französisch", variable=var, value=2).pack()
 
 middleFrame.pack()
 OptionMenu(middleFrame, printer, *printerList).pack()
+Label(middleFrame, text="Number of prints").pack(side=LEFT, padx=25)
+numberEntry = Entry(middleFrame, width=4)
+numberEntry.insert(0,'1')
+numberEntry.pack(side=LEFT)
+
 
 bottomFrame.pack()
 Button(bottomFrame, text="OK", command=quit_loop).pack(ipadx=15, side=LEFT, padx=10)
@@ -77,7 +83,8 @@ master.mainloop()
 pdfHeader = PdfFileReader(HEADER)
 ust = PdfFileReader(chosen_ust)
 ust = ust.getPage(0)
-printString = "lp -d " + selectedPrinter + " -o media=A4 -o fit-to-page -o duplex=none"
+printString = "lp -d " + selectedPrinter + " -o media=A4 -o fit-to-page -o duplex=none -n" + numberEntry.get()
+
 for filename in filenames:
     randomString = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(5))
     resultFileName = os.path.splitext(filename)[0] + '_' + randomString + '_EDITED.pdf'
@@ -105,5 +112,6 @@ for filename in filenames:
 
     os.remove(filename)
 
-os.system(printString)
+if(selectedPrinter != "PDF"):
+    os.system(printString)
 
